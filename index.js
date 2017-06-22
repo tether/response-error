55c6c92342213225dd6836d2e877ca5a3e09d598
bredele <olivier.wietrich@gmail.com>
2017-06-22 11:16:17 -0600

    Add simple error management

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+/**
+ * Dependencies.
+ */
+
+const status = require('response-status')
+
 
 /**
- * This is a simple description.
+ * Send status code to HTTP response.
  *
+ * @param {Stream} response
+ * @param {Error} error
  * @api public
  */
 
-module.exports = function () {
-  // do something
+module.exports = function (response, error) {
+  let code = error.statusCode
+  const name = error.name
+  if (!code) {
+    if (name === 'ReferenceError') code = 400
+    if (name === 'TypeError') code = 400
+  }
+  status(response, code || 500, error.message)
 }
-
-  
